592eaa5062847773e70f5e9be3e86146ec0b1e8e
Stephen P. McCormickgit status <mccormicks@wlu.edu>
2021-08-16 12:18:19 +0200

    Update FilmList.js

--- src/data/FilmList.js
+++ src/data/FilmList.js
@@ -11,36 +11,36 @@ export const FilmList = [
         title: "La bataille d'Alger",
         subtitle: "Un film de Gillo Pontecorvo (1966)",
         description: "Ce film parle de",
-        imgSrc: {Alger}
+        imgSrc: "https://github.com/SteveWLU/fren164/blob/main/src/images/alger.jpg"
     },
     {
         title: "Les parapluies de Cherbourg",
         subtitle: "Un film de Jacques Demy (1964)",
         description: "Ce film parle de",
-        imgSrc: {Cherbourg}
+        imgSrc: "https://github.com/SteveWLU/fren164/blob/main/src/images/cherbourg.jpg"
     },
     {
         title: "Cléo de 5 à 7",
         subtitle: "Un film d'Agnès Varda (1962)",
         description: "Ce film parle de",
-        imgSrc: {Cleo}
+        imgSrc: "https://github.com/SteveWLU/fren164/blob/main/src/images/cleo.jpg"
     },
     {
         title: "Les 400 coups",
         subtitle: "Un film de François Truffaut (1959)",
         description: "Ce film parle de",
-        imgSrc: {Coups}
+        imgSrc: "https://github.com/SteveWLU/fren164/blob/main/src/images/coups.png"
     },
     {
         title: "Hiroshima, mon amour",
         subtitle: "Un film d'Alain Renais (1959)",
         description: "Ce film parle de",
-        imgSrc: {Hiroshima}
+        imgSrc: "https://github.com/SteveWLU/fren164/blob/main/src/images/hiroshima.jpg"
     },
     {
         title: "À bout de souffle",
         subtitle: "Un film de Jean-Luc Godard (1960)",
         description: "Ce film parle de",
-        imgSrc: {Souffle}
+        imgSrc: "https://github.com/SteveWLU/fren164/blob/main/src/images/souffle.jpg"
     },
 ];
